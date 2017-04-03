c25c13f0efd4bc0ba63cd6cf67c7126df49e2657
Eric Mendoza-Conner <Trainlevel2@gmail.com>
2017-04-03 00:52:13 -0400

    Keep calm and classify on
    Needs some work, but can classify reasonably well if there is a key threshold.

---
+++ Software/classifier.m
@@ -0,0 +1,19 @@
+function output = classifier(input,errorMargin)
+    sum = input(0); count1 = 0;
+    sum2 = 0; count2 = 0;
+    x=1;
+    while(x<input.length)
+       if(abs((sum/(x-1))-input(x))<errorMargin*abs(input(x)))
+            sum = sum + input(x); count1 = count1 + 1;
+       else
+           sum2 = sum2 + input(x); count2 = count2 + 1;
+       end
+       x=x+1;
+    end
+    output = sum/count1;
+    if(sum2/count2 > output)
+        output = 0;
+    else
+        output = 1;
+    end
+end
